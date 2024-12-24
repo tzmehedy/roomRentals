85fc04766130465f2d761f525d4b93202e8defc4
unknown <mehedytouhidurzaman@gmail.com>
2024-12-25 00:32:17 +0600

    in the server side the mongo db set up is completed

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const cors = require("cors")
+require("dotenv").config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+
+const app = express();
 
 const port = process.env.PORT || 5000
-
-const app = express()
 
 app.use(cors())
 app.use(express.json())
@@ -11,6 +14,43 @@ app.use(express.json())
 app.get("/", (req,res)=>{
     res.send("Room rental is coming")
 })
+
+
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k8aq9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+
+    const roomsCollections = client.db("RoomRentals").collection("allRooms")
+
+    
+    
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.listen(port, ()=>{
     console.log(`The port is running is ${port}`)
